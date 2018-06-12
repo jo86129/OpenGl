5667612d60a159f735b2a3fb844b34b96e03568c
jo86129 <jo86129@gmail.com>
2018-06-12 13:01:08 +0800

    last version

---
+++ Rolling Ball/1042060_HW3/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _1042060_HW3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(0, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(457, 422);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.simpleOpenGlControl1_Paint);
+            this.simpleOpenGlControl1.Resize += new System.EventHandler(this.simpleOpenGlControl1_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(473, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "變快";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(473, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "變慢";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 422);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Rolling Ball/1042060_HW3/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+
+namespace _1042060_HW3
+{
+    public partial class Form1 : Form
+    {
+        double cx=0, cy=0, cz=0; //ball's position
+        double dx=2, dy=1, dz=3; //ball translate distance
+        double rot=3; //rotate angel
+        double radius = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            this.simpleOpenGlControl1.InitializeContexts();
+
+            Glut.glutInit();
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+            setViewingVolumn();
+            MyInt();
+        }
+        private void MyInt()
+        {
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        }
+        private void setViewingVolumn()
+        {
+            Gl.glViewport(0, 0, simpleOpenGlControl1.Size.Width,
+              simpleOpenGlControl1.Size.Height);
+
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            double aspect = (double)simpleOpenGlControl1.Size.Width /
+                            (double)simpleOpenGlControl1.Size.Height;
+
+            Glu.gluPerspective(45, aspect, 0.0, 20.0);
+        }
+        private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+           
+            Gl.glMatrixMode(Gl.GL_MODELVIEW); //這個矩陣是用來平移旋轉縮放
+            Gl.glLoadIdentity();
+            Glu.gluLookAt(0.0,0.0,20.0,0.0,0.0,0.0,0.0,1.0,0.0); //camera location  3-location  3-look at 3-攝影機的方向
+
+            //draw the walls
+            Gl.glBegin(Gl.GL_QUADS); //建議逆時針畫
+            //Gl.glColor3ub(255, 174, 201);     //pink
+            Gl.glColor3ub(239, 228, 176);     //yellow
+            Gl.glVertex3d(10.0, 10.0, -10.0);
+            Gl.glVertex3d(10.0, 10.0, 10.0);
+            Gl.glVertex3d(10.0, -10.0, 10.0);
+            Gl.glVertex3d(10.0,-10.0, -10.0);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_QUADS); //建議逆時針畫
+            Gl.glColor3ub(239,228,176);     //yellow
+            Gl.glVertex3d(-10.0, 10.0, -10.0);
+            Gl.glVertex3d(-10.0, 10.0, 10.0);
+            Gl.glVertex3d(-10.0, -10.0, 10.0);
+            Gl.glVertex3d(-10.0, -10.0, -10.0);
+            Gl.glEnd();
+
+
+            Gl.glBegin(Gl.GL_QUADS); //建議逆時針畫
+            Gl.glColor3ub(153,217,234);     //blue
+            Gl.glVertex3d(10.0, 10.0, -10.0);
+            Gl.glVertex3d(-10.0, 10.0, -10.0);
+            Gl.glVertex3d(-10.0, 10.0, 10.0);
+            Gl.glVertex3d(10.0, 10.0, 10.0);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_QUADS); //建議逆時針畫
+            Gl.glColor3ub(153, 217, 234);     //blue
+            Gl.glVertex3d(10.0, -10.0, -10.0);
+            Gl.glVertex3d(-10.0, -10.0, -10.0);
+            Gl.glVertex3d(-10.0, -10.0, 10.0);
+            Gl.glVertex3d(10.0, -10.0, 10.0);
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_QUADS); //建議逆時針畫
+            Gl.glColor3ub(200,191,231);     //purple
+            Gl.glVertex3d(10.0, 10.0, -10.0);
+            Gl.glVertex3d(-10.0, 10.0, -10.0);
+            Gl.glVertex3d(-10.0, -10.0, -10.0);
+            Gl.glVertex3d(10.0, -10.0, -10.0);
+            Gl.glEnd();
+
+
+            //the circle
+            Gl.glPushMatrix();
+            Gl.glColor3ub(243,95,121);//pink
+            Gl.glTranslated(cx,cy,cz);
+            Gl.glRotated(rot,1,1,1);
+            Glut.glutWireSphere(radius, 20, 20);
+            Gl.glPopMatrix();
+
+
+
+
+        }
+
+        private void simpleOpenGlControl1_Resize(object sender, EventArgs e)
+        {
+            setViewingVolumn();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (cx + radius >= 10 || cx - radius <= -10)
+            {
+                
+                dx = -dx;
+            }
+            if (cy + radius >= 10 || cy - radius <= -10)
+            {
+             
+                dy = -dy;
+            }
+            if (cz + radius >= 10 || cz - radius <= -10)
+            {
+               
+                dz = -dz;
+            }
+            rot += 3;
+            cx += dx;
+            cy += dy;
+            cz += dz;
+            this.simpleOpenGlControl1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timer1.Interval<=30)
+            {
+
+            }
+            else
+            {
+                timer1.Interval -= 30;
+                this.simpleOpenGlControl1.Refresh();
+            }
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Interval += 30;
+            this.simpleOpenGlControl1.Refresh();
+        }
+    }
+}
